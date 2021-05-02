bb6847c93d7d4cac9c2f410d0bb01f2913f04039
vaibhaviraut <vaibhaviraut09@gmail.com>
2021-05-01 21:47:42 -0400

    Commiting Changes.

--- UserInterface/assets/javascript/script.js
+++ UserInterface/assets/javascript/script.js
@@ -11,3 +11,50 @@ function openDay(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  function showEventDetails(){
+      var evd = document.getElementById("eventDetails");
+      if (evd.style.display === "none") {
+        evd.style.display = "block";
+      } else {
+        evd.style.display = "none";
+      }
+  }
+
+  var acc = document.getElementsByClassName("accordion");
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+		    acc[i].addEventListener("click", function() {
+		        /* Toggle between adding and removing the "active" class,
+		        to highlight the button that controls the panel */
+		        this.classList.toggle("active");
+
+		        /* Toggle between hiding and showing the active panel */
+		        var panel = this.nextElementSibling;
+		        if (panel.style.display === "block") {
+		            panel.style.display = "none";
+		        } else {
+		            panel.style.display = "block";
+		        }
+		    });
+		}
+
+    var subacc = document.getElementsByClassName("sub-accordion");
+		var i;
+
+		for (i = 0; i < subacc.length; i++) {
+		    subacc[i].addEventListener("click", function() {
+		        /* Toggle between adding and removing the "active" class,
+		        to highlight the button that controls the panel */
+		        this.classList.toggle("active");
+
+		        /* Toggle between hiding and showing the active panel */
+		        var subpanel = this.nextElementSibling;
+		        if (subpanel.style.display === "block") {
+		            subpanel.style.display = "none";
+		        } else {
+		            subpanel.style.display = "block";
+		        }
+		    });
+		}
