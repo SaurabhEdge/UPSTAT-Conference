c802a30fdefc762d47ca77685d0345c248a4262d
Saurabh Shukla <43504747+SaurabhEdge@users.noreply.github.com>
2021-05-08 18:02:40 -0400

    Update and rename registration.html to registration.php

---
+++ UserInterface/registration.php
@@ -0,0 +1,34 @@
+<?php
+$path ='./';
+$page = 'Registration';
+include $path.'assets/inc/header.php'
+?>
+
+        <section id="main-content" class="main-content">
+            <div class="main-content-intro"></div>
+            <div class="main-content-theme">
+                <h3>Registration</h3>
+                <p>
+                    UPSTAT goes fully virtual this year! After having to cancel UPSTAT 2020, we couldn't let another year go by without a weekend full of statistics.<br /><br />This year, our theme is inspired by the tumultuous year our home nation has
+                    had. We seek to explore how statistics can help us to serve social justice in terms of new methodology, using it to understand data in an applied way, and how to educate students to think critically about data.
+                </p>
+                <p>
+                    <!--Get the first date of conference with main activity such as keynote speaker-->
+                    <strong>April 23rd: </strong>Keynote Speaker
+                    <br/>
+                    <!--Get the second date of conference with general activities available-->
+                    <strong>April 24th: </strong>Short courses, parallel sessions and round tables
+                </p>
+
+                <a href="https://www.eventbrite.com/e/up-stat-2021-statistics-serving-social-justice-registration-134465777761#" id="register-button" target="_blank">
+                    <button class="button width-200"><span>Register now! </span></button>
+                </a>
+            </div>
+        </section>
+        <!-- <footer class="main-footer">
+            <p>@2021 UPSTAT created by Team SpideRIT</p>
+        </footer> -->
+    </div>
+</body>
+
+</html>
