c267de2df2fe5a3422eb9f137d53cd1080967c6b
Saurabh Shukla <43504747+SaurabhEdge@users.noreply.github.com>
2021-05-08 18:03:21 -0400

    Update and rename sponsors.html to sponsors.php

---
+++ UserInterface/sponsors.php
@@ -0,0 +1,51 @@
+<?php
+$path ='./';
+$page = 'Sponsors';
+include $path.'assets/inc/header.php'
+?>
+
+        <section id="main-content" class="main-content">
+            <div class="main-content-intro">
+            </div>
+            <div class="main-content-theme">
+                <h3>Sponsors</h3>
+                <ul>
+                    <li>
+                        <a href="https://www.amstat.org/">The American Statistical Association</a>
+                        <br/>
+                        <img src="./assets/media/asalogo.png" class="sponsor-image">
+                    </li>
+                    <li>
+                        <a href="https://www.rit.edu/liberalarts/department-criminal-justice">RIT Department of Criminal Justice</a>
+                        <br/>
+                        <img src="./assets/media/ritlogo.jpeg" class="sponsor-image">
+                    </li>
+                    <li>
+                        <a href="https://www.rit.edu/science/school-mathematical-sciences">Rochester Institute of Technology</a>
+                    </li>
+                    <li>
+                        <a href="https://www.wegmans.com/">Wegmans</a>
+                    </li>
+                    <li>
+                        <a href="https://foxpest-rochester.com/">Fox Pest Control – Rochester</a>
+                        <br/>
+                        <img src="./assets/media/foxpestcontrol.jpeg" class="sponsor-image">
+                    </li>
+                </ul>
+                <br/>
+                <p>
+                    <a href="https://community.amstat.org/rochester/events/upstats-2021/sponsors">Become a Sponsor</a> - The UPSTAT Conference series team is forever grateful to all the generous sponsors who have support the conference over the years.
+                    It’s extremely hard and newly impossible to properly run a high quality conference without the highly appreciated support of our sponsors. Learn more about becoming a sponsor.
+                </p>
+        </section>
+
+        <footer class="main-footer">
+            <p>
+                @2021 UPSTAT created by Team SpideRIT
+            </p>
+        </footer>
+        </div>
+
+</body>
+
+</html>
