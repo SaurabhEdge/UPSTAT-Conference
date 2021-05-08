d333c4927edbed5953f86db77862744956946dd9
Saurabh Shukla <43504747+SaurabhEdge@users.noreply.github.com>
2021-05-08 18:01:24 -0400

    Update and rename dataAnalyticsCompetition.html to dataAnalyticsCompetition.php

---
+++ UserInterface/dataAnalyticsCompetition.php
@@ -0,0 +1,30 @@
+<?php
+$path ='./';
+$page = 'Data Analytics Competition';
+include $path.'assets/inc/header.php'
+?>
+
+        <section id="main-content" class="main-content">
+            <div class="main-content-intro"></div>
+            <div class="main-content-theme">
+                <h3>Data Analytics Competition</h3>
+                <p>To further encourage student participation, we will hold a data analysis competition. A team of at least one student and no more than 5 students may enter the competition by submitting their entry information which includes team name,
+                    school, and the names and e-mail addresses of the team members. The data set will be accessible from the conference website on around March 10, 2021 to teams that have entered the competition. Teams will be required to submit a report
+                    (maximum of 10 pages) on their results by April 18, 2021. Prizes will be awarded to the top 3 teams, all of whom will be asked to present their results at the conference.
+
+                    <p>
+                        <h2>Important Links</h2>
+                        <a href="">Register Your Team</a>
+                        <br/>
+                        <a href="">Report Submission Form</a>
+                    </p>
+                </p>
+            </div>
+        </section>
+        <!-- <footer class="main-footer">
+            <p>@2021 UPSTAT created by Team SpideRIT</p>
+        </footer> -->
+    </div>
+</body>
+
+</html>
