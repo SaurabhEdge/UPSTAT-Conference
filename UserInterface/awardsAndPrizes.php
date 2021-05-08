47ae7373aadb59655c4f784bd510c33b3e571591
Saurabh Shukla <43504747+SaurabhEdge@users.noreply.github.com>
2021-05-08 18:00:50 -0400

    Update and rename awardsAndPrizes.html to awardsAndPrizes.php

---
+++ UserInterface/awardsAndPrizes.php
@@ -0,0 +1,30 @@
+<?php
+$path ='./';
+$page = 'Awards and Prizes';
+include $path.'assets/inc/header.php'
+?>
+
+        <section id="main-content" class="main-content">
+            <div class="main-content-intro">
+
+                <div class="main-content-theme">
+                    <h3>Awards and Prizes</h3>
+                    <p>
+                        All session organizers who are students will receive an UPSTAT 2021 Young Scientific Leader Award. Please submit the details of our organized session
+                        <a href="https://forms.gle/AqJ1JjaHaqnrzg1m9">here</a>. The deadline for submission of an organized session is <strong>Friday, April 9, 2021</strong>.
+
+                        <br /> Monetary prizes will be awarded to students for the best poster and oral presentations related to (1) statistical methods and (2) statistics/data science applications. Finally, a mentorship award will be presented to the
+                        faculty member who is a co-author on the highest number of student presentations.
+                    </p>
+                </div>
+        </section>
+        <!-- <footer class="main-footer">
+            <p>
+                @2021 UPSTAT created by Team SpideRIT
+            </p>
+        </footer> -->
+        </div>
+
+</body>
+
+</html>
