d2fa277d392333e27266085bd58dbf4f8f81205f
Saurabh Shukla <43504747+SaurabhEdge@users.noreply.github.com>
2021-05-08 18:00:16 -0400

    Update and rename abstractSubmission.html to abstractSubmission.php

---
+++ UserInterface/abstractSubmission.php
@@ -0,0 +1,27 @@
+<?php
+$path ='./';
+$page = 'Abstract Submission';
+include $path.'assets/inc/header.php'
+?>
+
+
+        <section id="main-content" class="main-content">
+            <div class="main-content-intro"></div>
+            <div class="main-content-theme">
+                <h3>Abstract Submissions</h3>
+                <p>We are currently seeking submissions for our parallel session from attendees of all levels! Undergraduates, graduates, faculty and professionals are all welcome! Please note that your contributions do not need to follow the theme, any
+                    topic is welcome. You are cordially invited to submit an abstract for presentation at the Ninth Joint Conference of the Upstate Chapters of the American Statistical Association, which will be held Online on Friday and Saturday, April
+                    23-24, 2021. If you are interested in giving a talk in our parallel sessions or organizing a session.</p>
+                <p>Abstracts must contain at most 250 words and should not contain any references. The deadline for the submission of the abstract is Friday, April 9, 2021, and notification of acceptance for a poster or oral presentation will be sent out
+                    on April 12, 2021. To submit your abstract, please go to the <a href="https://docs.google.com/forms/d/e/1FAIpQLSc2gjiATV-39rCAZLUXUSERBHFSVXnVvDPdJOY7nqEX2RMrUQ/viewform">form</a></p>
+                <p>For all questions regarding abstracts, please contact Conference Program Chair, Dr.Katherine Grzesik <a href="mailto:katherine.grzesik@gmail.com">katherine.grzesik@gmail.com</a> </p>
+                <button class="button width-200"><span>Abstract Submission </span></button>
+            </div>
+        </section>
+        <!-- <footer class="main-footer">
+            <p>@2021 UPSTAT created by Team SpideRIT</p>
+        </footer> -->
+    </div>
+</body>
+
+</html>
