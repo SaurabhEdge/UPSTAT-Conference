ab4615e7ae586b9f330583738b8ef1d448b4eb11
Saurabh Shukla <43504747+SaurabhEdge@users.noreply.github.com>
2021-05-08 18:03:50 -0400

    Update and rename tutorials.html to tutorials.php

---
+++ UserInterface/tutorials.php
@@ -0,0 +1,21 @@
+<?php
+$path ='./';
+$page = 'Tutorials';
+include $path.'assets/inc/header.php'
+?>
+
+
+        <section id="main-content" class="main-content">
+            <div class="main-content-intro"></div>
+            <div class="main-content-theme">
+                <h3>Tutorials</h3>
+                <p> The first day of the conference (Friday, April 23) will feature tutorials (workshops on topics of broad interest). As in previous years, there will be at least four (4) appealing and compelling tutorials featuring state of the art aspects
+                    of statistics, machine learning, artificial intelligence and data science, Please check the conference website for greater details. Tutorials will be provided free of charge to registered conference participants. Space is limited,
+                    so reservations for a seat soon.</p>
+                <button class="button width-150"><span>Submit Tutorials </span></button>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
