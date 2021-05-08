68324513d9d7e0da2765a55a82a3b5ffcfd0a52d
SaurabhEdge <ss7586@rit.edu>
2021-05-08 18:17:46 -0400

    My first inc version

---
+++ UserInterface/assets/inc/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page;?></title>
+    <link rel="stylesheet" href="./assets/css/styles.css" media="screen" />
+</head>
+
+<body>
+    <div class="container">
+        <section class="main-carousel">
+            <!-- <h1>Carousel</h1> width="1169"-->
+            <img src="./assets/media/UPSTAT_Banner.jpg" class="banner" alt="carousel-banner" />
+        </section>
+        <section id="site-navigation" class="main-navigation" role="navigation" aria-label="Site Menu">
+            <div class="menu-site-nav-container">
+                <div class="site-logo">
+                    <a href="#" class="custom-logo" rel="home">
+                        <img src="./assets/media/UpStat_Logo2.jpg" class="custom-logo" alt="UpSTATLogo" width="350" />
+                    </a>
+                </div>
+                <div class="drFokoue">
+                    <p>
+                        <h3> Dr. Ernest Fokoue</h3>
+                        General Conference Chair
+                        <br/>Mail to: <a href="mailto:epfeqa@rit.edu">epfeqa@rit.edu</a>
+                    </p>
+                </div>
+                <ul id="site-menu" class="menu">
+                    <li id="menu-item-1" class="Home nav-bar-active">
+                        <a href="index.php">Get To Know UPSTAT</a>
+                    </li>
+                    <li id="menu-item-2" class="Schedule">
+                        <a href="schedule.php">Schedule</a>
+                    </li>
+                    <li id="menu-item-3" class="Registration">
+                        <a href="registration.php">Registration</a>
+                    </li>
+                    <li id="menu-item-4" class="Tutorials">
+                        <a href="tutorials.php">Tutorials</a>
+                    </li>
+                    <li id="menu-item-5" class="Speakers">
+                        <a href="keyNoteSpeaker.php">Keynote Speakers</a>
+                    </li>
+                    <li id="menu-item-6" class="Abstract">
+                        <a href="abstractSubmission.php">Abstract Submission</a>
+                    </li>
+                    <li id="menu-item-7" class="Awards">
+                        <a href="awardsAndPrizes.php">Awards and Prizes</a>
+                    </li>
+                    <li id="menu-item-8" class="Competition">
+                        <a href="dataAnalyticsCompetition.php">Data Analytics Competition</a>
+                    </li>
+                    <li id="menu-item-9" class="Sponsors">
+                        <a href="sponsors.php">Sponsors</a>
+                    </li>
+                </ul>
+                <div class="footer-div">
+                    @2021 UPSTAT created by Team SpideRIT
+                </div>
+            </div>
+        </section>
